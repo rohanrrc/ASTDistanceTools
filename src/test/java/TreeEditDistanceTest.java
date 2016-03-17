46c4af85d3ccdb9acb0d1c42bd4d1055ca6f40c2
Rohan Roy Choudhury <rrc@berkeley.edu>
2016-03-16 17:57:47 -0700

    add test for number of elements in output

--- src/test/java/TreeEditDistanceTest.java
+++ src/test/java/TreeEditDistanceTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import tree.LblTree;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,4 +17,12 @@ public class TreeEditDistanceTest {
         LblTree lt = LblTree.fromString((new BufferedReader(new FileReader(ast))).readLine());
         assertEquals(0, new EditDist(true).nonNormalizedTreeDist(lt,lt), 0.001);
     }
+
+    @Test
+    public void LengthOfTreeEditArrayShouldEqualNumberOfFilesInDirectoryMinusOne() throws Exception {
+        Path home_dir = Paths.get("resources/python/test_subset/");
+        int num_files = new File(home_dir.resolve("ast").toUri()).list().length;
+        assertEquals(num_files - 1, new TreeEditDistanceArray(home_dir, "python").compute().length);
+    }
+
 }
